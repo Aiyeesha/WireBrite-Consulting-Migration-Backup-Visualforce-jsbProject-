2a9bbc41b7ad2ee1a4c568023e0afc4531ca033e
Aiyeesha <ai.dahoumane@gmail.com>
2024-11-24 12:16:39 +0100

    Hotfix

---
+++ .sfdx/typings/lwc/apex/AccountOpportunitiesController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/AccountOpportunitiesController.getOpportunities" {
+  export default function getOpportunities(param: {accountId: any, searchTerm: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/apex/LeadStatusController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/LeadStatusController.updateStatus" {
+  export default function updateStatus(param: {leadId: any, status: any}): Promise<any>;
+}
